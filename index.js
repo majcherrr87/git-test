d2eabcc80b44dadd9edd80a9044e80c498f21f16
Adrian Majcher <majcherrr87@gmail.com>
2024-01-18 16:31:51 +0100

    new console.log

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 console.log(`hello Word`);
 console.log(`hello Word 2`);
 console.log(`hello Word 3`);
+console.log(`hello Word 4`);
+console.log(`hello Word 5`);
+console.log(`hello Word 6`);
+console.log(`hello Word 7`);
+console.log(`hello Word 8`);
